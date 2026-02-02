fc13f8063d02c67fc0534dc6e7a2126be6c48b11
Miguel68 <miguelsilleroficial@gmail.com>
2026-02-01 21:43:59 -0300

    Criação fluxo register senha com hash e autenticação

--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,5 @@
 from app.extensions import db
+import bcrypt
 
 
 class User(db.Model):
@@ -13,3 +14,16 @@ class User(db.Model):
     password = db.Column(db.String(200), nullable=False)
 
     create_date = db.Column(db.DateTime, server_default=db.func.now())
+
+    def hash_password(self, password_digitada):
+        hash_pass = bcrypt.hashpw(
+            password_digitada.encode('utf-8'),
+            bcrypt.gensalt()
+        )
+        self.password = hash_pass.decode('utf-8')
+
+    def check_pass(self, password_digitada):
+        return bcrypt.checkpw(
+            password_digitada.encode('utf-8'),
+            self.password.encode('utf-8')
+        )
--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -8,7 +8,7 @@ user_bp = Blueprint("user", __name__)
 @user_bp.route("/register", methods=["POST"])
 def register():
     data = request.get_json()
-    
+
     name = data.get("name")
     email = data.get("email")
     password = data.get("password")
@@ -23,11 +23,32 @@ def register():
 
     novo_user = User(
         name=name,
-        email=email,
-        password=password
+        email=email
     )
+    novo_user.hash_password(password)
 
     db.session.add(novo_user)
     db.session.commit()
 
     return jsonify({"mensagem": "User criado com sucesso"}), 200
+
+
+@user_bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+
+    email = data.get("email")
+    password = data.get("password")
+
+    if not email or not password:
+        return jsonify({"erro": "Email ou senha não fornecidos"})
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        return jsonify({"erro": "Email não encontrado"}), 401    
+
+    if not user.check_pass(password):
+        return jsonify({"erro": "Senha incorreta"}), 401
+
+    return jsonify({"mensagem": f"Usuaria autentica - Ola {user.name}"}), 200
