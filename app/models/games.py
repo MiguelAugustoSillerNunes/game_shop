2570f449830ee587b73166aff9454a71274463f4
Miguel68 <miguelsilleroficial@gmail.com>
2026-02-01 22:19:14 -0300

    Criação de Model Games

---
+++ app/models/games.py
@@ -0,0 +1,23 @@
+from app.extensions import db
+'''
+Model Games - cria tabela games no banco - armazena preço, versão, console, ativo (Y/N)
+Valor Padrão e ativo = Y e de preço = 0
+'''
+
+
+class Games(db.Model):
+    __tablename__ = "games"
+
+    id = db.Column(db.Interger, primary_key=False)
+
+    name = db.Column(db.String(100), nullable=False)
+
+    version = db.Column(db.String(20), nullable=False)
+
+    price = db.Column(db.Numeric(5, 2), nullable=False, server_default=db.text("0"), default=0)
+
+    description = db.Column(db.String(300), nullable=False)
+
+    console = db.Column(db.String(15), nullable=False)
+
+    activated = db.Column(db.String(1), nullable=False, server_default=db.text("'Y'"))
