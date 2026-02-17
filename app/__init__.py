df83e524f082715813386238a77de47f2b7dedd6
MiguelNunes <miguelsilleroficial@gmail.com>
2026-02-16 23:50:24 -0300

    add flask_cors

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from .config import Config
 from .extensions import db
 from .routes.user_routes import user_bp
@@ -9,6 +10,7 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     app.json.sort_keys = False
+    CORS(app, origins=["http://127.0.0.1:5500"])
     app.register_blueprint(user_bp)
     app.register_blueprint(games_bp)
     db.init_app(app)
